28398217a320b94bad91aa589758946ad1f48882
KrasimirKostadinov <krasimir.kostadinov@modis.com>
2023-02-17 16:56:20 +0200

    added useFormHook

--- src/pages/Register/components/RegisterForm.tsx
+++ src/pages/Register/components/RegisterForm.tsx
@@ -1,4 +1,13 @@
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+   username: string;
+   password: string;
+};
+
 const RegisterForm: React.FC = () => {
+   const { handleSubmit } = useForm<Inputs>();
+
    return <form>test</form>;
 };
 
