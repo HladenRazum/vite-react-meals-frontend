9954b1d10ae55da65bf5e68ce7b1b8e5fe623152
KrasimirKostadinov <krasimir.kostadinov@modis.com>
2023-02-22 20:49:10 +0200

    small updates on Filter

--- src/components/Layout/Filter/Filter.tsx
+++ src/components/Layout/Filter/Filter.tsx
@@ -1,5 +1,39 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import "./Filter.scss";
+
+type FilterItem = {
+   id: string;
+   name: string;
+   label: string;
+};
+
+const FILTERS: FilterItem[] = [
+   {
+      id: "alcohol",
+      name: "alcohol",
+      label: "Алкохол",
+   },
+   {
+      id: "banitsa",
+      name: "banitsa",
+      label: "Баница",
+   },
+   {
+      id: "garnish",
+      name: "garnish",
+      label: "Гарнитури",
+   },
+   {
+      id: "lamb-meat",
+      name: "lamb-meat",
+      label: "Агнешко месо",
+   },
+   {
+      id: "dairy",
+      name: "dairy",
+      label: "Млечни продукти",
+   },
+];
 
 const Filter: React.FC = () => {
    const [isVisible, setIsVisible] = useState(false);
@@ -13,12 +47,36 @@ const Filter: React.FC = () => {
    // 2. Try displaying only the first 3 filter items (as a subarray);
    // 3. On Show more click, display the rest of the elements
 
+   const handleSubmit = (e: FormEvent) => {
+      console.log(e);
+      // Send a request to server and update the displayed items depending on the applied filters
+
+      // show selected items
+   };
+
    return (
       <div className="Filter test-border">
          <h3 className="title border-bottom">Категория</h3>
-         <button onClick={toggleVisible}>Show more</button>
-         <form>
-            <div className="filter-item">
+         {FILTERS.length > 0 ? (
+            <button onClick={toggleVisible}>Show more</button>
+         ) : (
+            <p>No results</p>
+         )}
+         <form onSubmit={handleSubmit}>
+            {FILTERS.length > 0 &&
+               FILTERS.map((filter) => (
+                  <div key={filter.id} className="filter-item">
+                     <label htmlFor={filter.id}>{filter.label}</label>
+                     <input
+                        type="checkbox"
+                        name={filter.name}
+                        id={filter.id}
+                        onChange={handleSubmit}
+                        value={filter.name}
+                     />
+                  </div>
+               ))}
+            {/* <div className="filter-item">
                <label htmlFor="alcohol">Алкохол</label>
                <input type="checkbox" name="alcohol" id="alcohol" />
             </div>
@@ -41,7 +99,7 @@ const Filter: React.FC = () => {
             <div className="filter-item">
                <label htmlFor="salad">Салати</label>
                <input type="checkbox" name="salad" id="salad" />
-            </div>
+            </div> */}
          </form>
       </div>
    );
