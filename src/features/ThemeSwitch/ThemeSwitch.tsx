625855a584cc1d8b8aa36fbef2bfaa50392d9e5f
KrasimirKostadinov <krasimir.kostadinov@modis.com>
2023-02-27 11:15:04 +0200

    added default selected option for current theme

--- src/features/ThemeSwitch/ThemeSwitch.tsx
+++ src/features/ThemeSwitch/ThemeSwitch.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent } from "react";
-import { useDispatch } from "react-redux";
-import { changeTheme } from "../../store/user/userSlice";
+import { ChangeEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeTheme, getTheme } from "../../store/user/userSlice";
 import { Theme } from "../../types/theme.type";
 import classes from "./ThemeSwitch.module.scss";
 
 const ThemeSwitch: React.FC = () => {
+   const theme = useSelector(getTheme);
    const dispatch = useDispatch();
 
    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -12,35 +13,41 @@ const ThemeSwitch: React.FC = () => {
    };
 
    return (
-      <form className={classes.ThemeSwitch}>
-         <div className={classes.row}>
-            <input
-               id="light"
-               type="radio"
-               name="theme"
-               onChange={handleChange}
-            />
-            <label htmlFor="light">Light theme</label>
-         </div>
-         <div className={classes.row}>
-            <input
-               id="dark"
-               type="radio"
-               name="theme"
-               onChange={handleChange}
-            />
-            <label htmlFor="dark">Dark theme</label>
-         </div>
-         <div className={classes.row}>
-            <input
-               id="high-contrast"
-               type="radio"
-               name="theme"
-               onChange={handleChange}
-            />
-            <label htmlFor="high-contrast">High-contrast theme</label>
-         </div>
-      </form>
+      <div className={classes.ThemeSwitch}>
+         <h5>Themes</h5>
+         <form>
+            <div className={classes.row}>
+               <input
+                  id="light"
+                  type="radio"
+                  name="theme"
+                  onChange={handleChange}
+                  checked={theme === "light"}
+               />
+               <label htmlFor="light">Light</label>
+            </div>
+            <div className={classes.row}>
+               <input
+                  id="dark"
+                  type="radio"
+                  name="theme"
+                  onChange={handleChange}
+                  checked={theme === "dark"}
+               />
+               <label htmlFor="dark">Dark</label>
+            </div>
+            <div className={classes.row}>
+               <input
+                  id="high-contrast"
+                  type="radio"
+                  name="theme"
+                  onChange={handleChange}
+                  checked={theme === "high-contrast"}
+               />
+               <label htmlFor="high-contrast">High-contrast</label>
+            </div>
+         </form>
+      </div>
    );
 };
 
