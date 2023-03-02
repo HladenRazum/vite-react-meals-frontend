9c804457e60c9007aea363ff2acd62888b256caf
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2023-03-02 21:00:35 +0200

    added useForm hook to NewRecipe form

--- src/pages/NewRecipe/components/NewRecipeForm/NewRecipeForm.tsx
+++ src/pages/NewRecipe/components/NewRecipeForm/NewRecipeForm.tsx
@@ -1,35 +1,56 @@
 import { FormEvent } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormData = {
+   recipeName: string;
+   ingredients: string;
+   cookingInstrictions: string;
+};
 
 const NewRecipeForm: React.FC = () => {
-   const handleSubmit = (e: FormEvent) => {
-      e.preventDefault();
-      console.log(e.target);
+   const { register, handleSubmit, reset } = useForm<FormData>();
+
+   const onSubmit: SubmitHandler<FormData> = ({
+      recipeName,
+      ingredients,
+      cookingInstrictions,
+   }) => {
+      console.log({ recipeName, ingredients, cookingInstrictions });
+      reset();
    };
 
    return (
-      <form className="form wide">
+      <form className="form wide" onSubmit={handleSubmit(onSubmit)}>
          <div className="formGroup">
-            <label htmlFor="name">Recipe name</label>
-            <input type="text" id="name" placeholder="name" />
+            <label htmlFor="recipe-name">Recipe name</label>
+            <input
+               type="text"
+               id="recipe-name"
+               placeholder="Recipe Name"
+               {...register("recipeName")}
+            />
          </div>
          <div className="formGroup">
             <label htmlFor="ingredients">Ingredients</label>
             <textarea
                id="ingredients"
-               placeholder="ingredients"
+               placeholder="Ingredients"
                className="non-resizable-x"
                rows={5}
+               {...register("ingredients")}
             />
          </div>
          <div className="formGroup">
-            <label htmlFor="cooking-instructions">Ingredients</label>
+            <label htmlFor="cooking-instructions">Cooking Instructions</label>
             <textarea
                id="cooking-instructions"
                placeholder="Cooking instructions"
                className="non-resizable-x"
                rows={5}
+               {...register("cookingInstrictions")}
             />
          </div>
+         <button type="submit">Submit</button>
       </form>
    );
 };
